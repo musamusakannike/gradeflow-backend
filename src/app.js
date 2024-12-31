9cf964f2c3729c71ecf726973ebae30ca693a158
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2024-12-31 07:28:47 +0100

    Created entry points

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const connectDB = require("./config/db.config");
+
+const authRoutes = require('./routes/auth.route');
+const classRoutes = require('./routes/class.route');
+
+const app = express();
+connectDB();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
+
+// Test server
+app.get("/", (req, res) => {
+  res.json({ status: "success", message: "Welcome to the API", data: null });
+});
+
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/class", classRoutes);
+
+module.exports = app;
